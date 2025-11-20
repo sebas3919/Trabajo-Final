072670892b309b2d0e075b5f7b92b710ab334bdd
Andres <gaaradesune@gmail.com>
2025-11-20 18:22:33 -0500

    aaaa

--- Docente.cs
+++ Docente.cs
@@ -16,5 +16,10 @@ namespace Trabajo_Final
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
